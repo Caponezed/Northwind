d377673b14bafbacc558dcb916beb3ab4c42f09e
Anton Karpov <slam.caponezed@mail.ru>
2024-03-12 13:23:08 +0300

    Product model is created

---
+++ northwind.client/src/app/Models/Product.ts
@@ -0,0 +1,14 @@
+export default class Product {
+  constructor(
+    public productId?: number,
+    public productName?: string,
+    public quantityPerProduct?: string,
+    public unitsInStock?: number,
+    public productCategory?: string,
+    public productDescription?: string,
+    public productPrice?: number,
+    public unitsOnOrder?: number,
+    public reorderLevel?: number,
+    public discounted?: number
+  ) {}
+}
