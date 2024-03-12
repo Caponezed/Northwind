47521c7a30ec5aafed697b3addef2b47615f48b9
Anton Karpov <slam.caponezed@mail.ru>
2024-03-12 14:07:05 +0300

    Products controller is created

---
+++ Northwind.Server/Controllers/ProductsController.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace Northwind.Server.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class ProductsController : ControllerBase
+    {
+        private readonly NorthwindContext _context;
+
+        public ProductsController(NorthwindContext context)
+        {
+            this._context = context;
+        }
+        
+        [HttpGet(Name = "GetAllProducts")]
+        public async Task<ActionResult<List<Object>>> GetAllProducts()
+        {
+            var products = _context.Products
+                .Join(
+                _context.Categories,
+                p => p.CategoryId,
+                c => c.CategoryId,
+                (p, c) => new
+                {
+                    productId = p.ProductId,
+                    productName = p.ProductName,
+                    productCategory = c.CategoryName,
+                    productDescription = c.Description,
+                    productPrice = p.UnitPrice,
+                    quantityPerProduct = p.QuantityPerUnit,
+                    unitsInStock = p.UnitsInStock,
+                }).ToListAsync<Object>();
+                
+            return Ok(await products);
+        }
+    }
+}
