2af7ebc720c1cfdf2a347dd02e4818b1c540dc8e
Anton Karpov <slam.caponezed@mail.ru>
2024-03-15 23:09:06 +0300

    Filter method is added to the products component

--- northwind.client/src/app/products/products.component.ts
+++ northwind.client/src/app/products/products.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit, Output, SimpleChanges } from '@angular/core';
 import Product from '../Models/Product';
 import { FetchingService } from '../services/fetching.service';
 import Category from '../Models/Category';
@@ -12,27 +12,43 @@ export class ProductsComponent implements OnInit {
   constructor(private fetchingService: FetchingService) {}
 
   title: string = 'Our products';
-  products?: Product[];
-  categories?: Category[];
 
-  ngOnInit(): void {
-    this.getAllProducts();
+  @Input() products?: Product[];
+
+  selectedCategory: string = 'All';
+
+  categories?: Category[];
+  visibleProducts?: Product[];
+
+  ngOnInit() {
     this.getAllCategories();
   }
 
-  getAllProducts(): void {
-    this.fetchingService
-      .getAllItems<Product[]>('products')
-      .subscribe((products) => {
-        this.products = products;
-      });
+  ngOnChanges() {
+    this.filterProducts(this.selectedCategory);
   }
 
-  getAllCategories(): void {
+  getAllCategories() {
     this.fetchingService
       .getAllItems<Category[]>('categories')
       .subscribe((categories) => {
         this.categories = categories;
       });
+  }
+
+  filterProducts(selectedCategory: string = this.selectedCategory): void {
+    const exCategory = this.selectedCategory;
+    const newCategory = selectedCategory;
+
+    this.selectedCategory = newCategory;
+
+    if (this.selectedCategory === 'All' || exCategory === newCategory) {
+      this.visibleProducts = this.products;
+      return;
+    }
+
+    this.visibleProducts = this.products?.filter((product) => {
+      return product.productCategory === selectedCategory;
+    });
   }
 }
